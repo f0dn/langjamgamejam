39163c2b9c56a2df688359dd2ee0c8f5cff484fd
Flint Mueller <flintthecuber@gmail.com>
2025-12-14 12:13:36 -0500

    tile start

---
+++ app/src/main/java/langgame/app/Tile.java
@@ -0,0 +1,13 @@
+package langgame.app;
+
+class Tile {
+    private String type;
+
+    public Tile(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
