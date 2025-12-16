c037c8beaa4a155827435e222387455861208727
Flint Mueller <flintthecuber@gmail.com>
2025-12-16 15:10:02 -0500

    keyboard input

--- app/src/main/java/langgame/app/App.java
+++ app/src/main/java/langgame/app/App.java
@@ -1,7 +1,10 @@
 package langgame.app;
 
+import java.awt.event.ActionEvent;
 import java.util.HashMap;
 
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
 import javax.swing.SwingUtilities;
 
 public class App {
@@ -11,25 +14,47 @@ public class App {
             public void run() {
                 TileTypes tileTypes = new TileTypes();
                 String[][] initialTiles = {
-                    {"grass", "water", "sand"},
-                    {"sand", "grass", "water"},
-                    {"water", "sand", "grass"}
+                        { "grass", "water", "sand" },
+                        { "sand", "grass", "water" },
+                        { "water", "sand", "grass" }
                 };
                 Grid grid = new Grid(initialTiles, tileTypes);
                 Entity player = new Entity();
                 HashMap<String, Entity> entities = new HashMap<>();
                 entities.put("player", player);
                 Window window = new Window("testing", grid, entities);
-                window.attachKeyListener(
-                    javax.swing.KeyStroke.getKeyStroke("UP"),
-                    new javax.swing.AbstractAction() {
-                        @Override
-                        public void actionPerformed(java.awt.event.ActionEvent e) {
-                            player.move(0, -1);
-                            window.repaint();
-                        }
-                    }
-                );
+                window.addKeyHeldListener(
+                        KeyStroke.getKeyStroke("UP"),
+                        new AbstractAction() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                player.move(0, -1);
+                            }
+                        });
+                window.addKeyHeldListener(
+                        KeyStroke.getKeyStroke("DOWN"),
+                        new AbstractAction() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                player.move(0, 1);
+                            }
+                        });
+                window.addKeyHeldListener(
+                        KeyStroke.getKeyStroke("LEFT"),
+                        new AbstractAction() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                player.move(-1, 0);
+                            }
+                        });
+                window.addKeyHeldListener(
+                        KeyStroke.getKeyStroke("RIGHT"),
+                        new AbstractAction() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                player.move(1, 0);
+                            }
+                        });
             }
         });
     }
